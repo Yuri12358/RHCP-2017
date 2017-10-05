9ba6bcec1ac3cb46f45f1f739330b13bccf27b36
theydiditforlulz <theydiditforlulz@kekeke.kekeke>
2017-10-05 18:13:53 +0300

    Added better wire connection drawing

--- src/ComponentRenderer.cpp
+++ src/ComponentRenderer.cpp
@@ -48,25 +48,101 @@ void ComponentRenderer::drawCurrent() {
 
 void ComponentRenderer::m_drawWire(nlohmann::json & pin) {
 	int cellsize = JSONHolder::get()["settings"]["cellsize"];
-	sf::VertexArray wire(sf::LineStrip, 2);
+
+	sf::VertexArray wire(sf::LineStrip, 4);
 	std::string parentID = pin["parentID"];
+
 	nlohmann::json & parent = JSONHolder::get()["field"]["contents"]
 		[parentID];
+
 	sf::Vector2f pos = sf::Vector2f(static_cast<int>(parent["position"]
 		["x"]) + static_cast<int>(pin["x"]), static_cast<int>(parent
 		["position"]["y"]) + static_cast<int>(pin["y"]));
+
 	wire[0].position = pos * float(cellsize);
+
 	nlohmann::json & conn = pin["connection"];
 	std::string otherParentID = conn["parentID"];
+	
 	nlohmann::json & other = JSONHolder::get()["field"]["contents"]
 		[otherParentID];
+	
 	nlohmann::json & otherPin = *(ComponentInfo::getComponentPin(
 		conn["parentID"], conn["x"], conn["y"]));
 	sf::Vector2f otherPos = sf::Vector2f(static_cast<int>(other["position"]
 		["x"]) + static_cast<int>(otherPin["x"]), static_cast<int>(other
 		["position"]["y"]) + static_cast<int>(otherPin["y"]));
-	wire[1].position = otherPos * float(cellsize);
-	wire[0].color = wire[1].color = sf::Color::Black;
+	
+	// There will be a lot of "if's"
+	
+	Direction dir1 = ComponentInfo::getPinOrientation(pin), dir2 = ComponentInfo::getPinOrientation(otherPin);
+
+	if((dir1 & (Left | Right)) && (dir1 & getOppositeDirection(dir2))) {
+		sf::Vector2f left = otherPos, right = pos;
+		if ((dir1 & Left) && !(dir2 & Left)) {
+			left = pos;
+			right = otherPos;
+		}
+		if ((left.x - right.x) > 0) {
+			wire[1].position = sf::Vector2f((pos.x + otherPos.x) / 2, pos.y) * float(cellsize);
+			wire[2].position = sf::Vector2f((pos.x + otherPos.x) / 2, otherPos.y) * float(cellsize);
+		} else {
+			wire[1].position = sf::Vector2f(pos.x, (pos.y + otherPos.y) / 2) * float(cellsize);
+			wire[2].position = sf::Vector2f(otherPos.x, (pos.y + otherPos.y) / 2) * float(cellsize);
+		}
+	}
+	else if((dir1 & (Up | Down)) && (dir1 & getOppositeDirection(dir2))) {
+		sf::Vector2f up = otherPos, down = pos;
+		if((dir1 & Up) && !(dir2 & Up)) {
+			up = pos;
+			down = otherPos;
+		}
+		if((up.y - down.y) > 0) {
+			wire[1].position = sf::Vector2f(pos.x, (pos.y + otherPos.y) / 2) * float(cellsize);
+			wire[2].position = sf::Vector2f(otherPos.x, (pos.y + otherPos.y) / 2) * float(cellsize);
+		} else {
+			wire[1].position = sf::Vector2f((pos.x + otherPos.x) / 2, pos.y) * float(cellsize);
+			wire[2].position = sf::Vector2f((pos.x + otherPos.x) / 2, otherPos.y) * float(cellsize);
+		}
+	}
+	else if((dir1 & (Left | Right)) && (dir1 & dir2))
+	{
+		sf::Vector2f left = otherPos, right = pos;
+		if((dir1 & Right))
+		{
+			left = pos;
+			right = otherPos;
+		}
+		if((right.x - left.x) > 0)
+		{
+			wire[1].position = wire[2].position = sf::Vector2f(otherPos.x, pos.y) * float(cellsize);
+		}
+		else
+		{
+			wire[1].position = wire[2].position = sf::Vector2f(pos.x, otherPos.y) * float(cellsize);
+		}
+	}
+	else if((dir1 & (Up | Down)) && (dir1 & dir2))
+	{
+		sf::Vector2f up = otherPos, down = pos;
+		if((dir1 & Down))
+		{
+			up = pos;
+			down = otherPos;
+		}
+		if((up.x - down.x) > 0)
+		{
+			wire[1].position = wire[2].position = sf::Vector2f(pos.x, otherPos.y) * float(cellsize);
+		}
+		else
+		{
+			wire[1].position = wire[2].position = sf::Vector2f(otherPos.x, pos.y) * float(cellsize);
+		}
+	}
+
+	wire[3].position = otherPos * float(cellsize);
+	
+	wire[0].color = wire[1].color = wire[2].color = wire[3].color = sf::Color::Black;
 	otherPin["connection"]["rendered"] = true;
 	App::get().window().draw(wire);
 }
